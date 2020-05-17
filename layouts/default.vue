975477209d37429c78a26bfbd1e10ac08fe00274
m.katayori <m.katayori@appre-marche.com>
2020-05-17 17:01:09 +0900

    サイドバーの一切替機能をオフにする

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,24 +30,28 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-btn
+
+      <!-- <v-btn
         icon
         @click.stop="clipped = !clipped"
       >
         <v-icon>mdi-application</v-icon>
-      </v-btn>
+      </v-btn> -->
+
       <v-btn
         icon
         @click.stop="fixed = !fixed"
       >
         <v-icon>mdi-minus</v-icon>
       </v-btn>
+      
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn
@@ -92,13 +96,13 @@
 export default {
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
       fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Top',
           to: '/'
         },
         // {
