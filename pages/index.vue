da23607b235df86947373f6705db714f4cda0c9a
m.katayori <m.katayori@appre-marche.com>
2020-05-17 17:02:31 +0900

    インデントをタブに変換

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <Logo />
-    <VuetifyLogo />
-  </div>
+	<div>
+		<Logo />
+		<VuetifyLogo />
+	</div>
 </template>
 
 <script>
@@ -10,9 +10,9 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+	components: {
+		Logo,
+		VuetifyLogo
+	}
 }
 </script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,3 @@
 <template>
-    <p>test</p>
+	<p>test</p>
 </template>
